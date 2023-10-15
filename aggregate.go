0fbd51a96da7a6a76a6a97145cb48c7d470e9c51
Adarsh A <adarsh.a@thoughtworks.com>
2023-10-15 12:31:39 +0530

    refactor: aggregate logic and increase unit test coverage
    Signed-off-by: Adarsh A <adarsh.a@thoughtworks.com>

--- aggregate.go
+++ aggregate.go
@@ -3,74 +3,66 @@ package collections
 // Returns the average of all elements present in the given collection.
 // This method supports only numerical types.
 func Avg[V Number](c Collection[V]) V {
-	switch c.Type() {
-	case TypeList:
-		size := V(c.Size())
-		if size == 0 {
-			return 0
-		}
-		return Sum[V](c) / size
+	size := V(c.Size())
+	if size == 0 {
+		return 0
 	}
-	panic("unknown collection type")
+	return Sum[V](c) / size
 }
 
 // Returns maximum element present in the given collection.
 // This method supports only numerical types.
 func Max[V Number](c Collection[V]) V {
-	switch c.Type() {
-	case TypeList:
-		l := c.(*List[V])
-		if l.Size() == 0 {
-			return 0
-		}
-		items := l.ToArray()
-		max := items[0]
-		for i := 1; i < l.Size(); i++ {
-			if max < items[i] {
-				max = items[i]
-			}
-		}
-		return max
+	slice := toSlice[V](c)
+	size := len(slice)
+	if size == 0 {
+		return 0
 	}
-	panic("unknown collection type")
+	max := slice[0]
+	for i := 1; i < size; i++ {
+		if max < slice[i] {
+			max = slice[i]
+		}
+	}
+
+	return max
 }
 
 // Returns minimum element present in the given collection.
 // This method supports only numerical types.
 func Min[V Number](c Collection[V]) V {
-	switch c.Type() {
-	case TypeList:
-		l := c.(*List[V])
-		if l.Size() == 0 {
-			return 0
-		}
-		items := l.ToArray()
-		min := items[0]
-		for i := 1; i < l.Size(); i++ {
-			if min > items[i] {
-				min = items[i]
-			}
-		}
-		return min
+	slice := toSlice[V](c)
+	size := len(slice)
+	if size == 0 {
+		return 0
 	}
-	panic("unknown collection type")
+	min := slice[0]
+	for i := 1; i < size; i++ {
+		if min > slice[i] {
+			min = slice[i]
+		}
+	}
+
+	return min
 }
 
 // Returns the sum of all elements present in the given collection.
 // This method supports only numerical types.
 func Sum[V Number](c Collection[V]) V {
+	slice := toSlice[V](c)
+	sum := V(0)
+	for _, elem := range slice {
+		sum += elem
+	}
+	return sum
+}
+
+func toSlice[V Number](c Collection[V]) []V {
 	switch c.Type() {
 	case TypeList:
-		l := c.(*List[V])
-		if l.Size() == 0 {
-			return 0
-		}
-		items := l.ToArray()
-		sum := items[0]
-		for i := 1; i < l.Size(); i++ {
-			sum += items[i]
-		}
-		return sum
+		return c.(*List[V]).ToArray()
+	case TypeSet:
+		return c.(*Set[V]).ToArray()
 	}
 	panic("unknown collection type")
 }
